cab3f9ba9224657a8291f6a8c1388b75e9f3bd66
linbojin <linbojin203@gmail.com>
2016-11-10 15:33:30 +0800

    feat: add shell script to install hadoop on ami

---
+++ setup-hadoop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing Oracle Java 8 ..."
+cd ~
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+sudo yum localinstall jdk-8u60-linux-x64.rpm
+rm ~/jdk-8u60-linux-x64.rpm
+
+echo "Installing Hadoop ..."
+wget http://archive.apache.org/dist/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz && \
+  tar -zxf hadoop-2.7.3.tar.gz && \
+  rm hadoop-2.7.3.tar.gz && \
+  mv hadoop-2.7.3 ~/hadoop
+
+mv -f conf/hadoop/* ~/hadoop/etc/hadoop/
+
+hdfs namenode -format
+
+echo 'export HADOOP_HOME=~/hadoop\nexport HADOOP_OPTS=-Djava.library.path=~/hadoop/lib/native\nexport PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
+source .bashrc
+
+echo "Generating ssh key"
+ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+echo "Starting Hdaoop ..."
+start-dfs.sh
+hdfs dfs -ls /
