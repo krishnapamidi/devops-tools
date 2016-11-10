e70fb4ea6abe36caeb9fac560d56c876a73c1f12
linbojin <linbojin203@gmail.com>
2016-11-10 15:40:16 +0800

    fix: make setup-hadoop.sh executable

--- setup-hadoop.sh
+++ setup-hadoop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing Oracle Java 8 ..."
+sourceDir=`pwd`
 cd ~
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
 sudo yum localinstall jdk-8u60-linux-x64.rpm
@@ -12,12 +13,11 @@ wget http://archive.apache.org/dist/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz
   rm hadoop-2.7.3.tar.gz && \
   mv hadoop-2.7.3 ~/hadoop
 
-mv -f conf/hadoop/* ~/hadoop/etc/hadoop/
-
-hdfs namenode -format
-
+echo "Initial Hadoop ..."
+mv -f $sourceDir/conf/hadoop/* ~/hadoop/etc/hadoop/
 echo 'export HADOOP_HOME=~/hadoop\nexport HADOOP_OPTS=-Djava.library.path=~/hadoop/lib/native\nexport PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
-source .bashrc
+source ~/.bashrc
+hdfs namenode -format
 
 echo "Generating ssh key"
 ssh-keygen -t rsa
