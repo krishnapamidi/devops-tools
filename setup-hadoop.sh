ebc63c0108f8221e51af0a01db465308b68e2f9a
linbojin <linbojin203@gmail.com>
2016-11-10 15:53:27 +0800

    fix: add jre installation

--- setup-hadoop.sh
+++ setup-hadoop.sh
@@ -3,7 +3,14 @@
 echo "*** Installing Oracle Java 8 ..."
 sourceDir=`pwd`
 cd ~
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+# Oracle Java 8 JRE
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+  "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
+sudo yum localinstall jre-8u60-linux-x64.rpm
+rm ~/jre-8u60-linux-x64.rpm
+# Oracle Java 8 JDK
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+  "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
 sudo yum localinstall jdk-8u60-linux-x64.rpm
 rm ~/jdk-8u60-linux-x64.rpm
 
@@ -14,7 +21,7 @@ wget http://archive.apache.org/dist/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz
   mv hadoop-2.7.3 ~/hadoop
 
 echo "*** Configuring Hadoop ..."
-mv -f $sourceDir/conf/hadoop/* ~/hadoop/etc/hadoop/
+cp -f $sourceDir/conf/hadoop/* ~/hadoop/etc/hadoop/
 echo 'export HADOOP_HOME=~/hadoop' >> ~/.bashrc
 echo 'export HADOOP_OPTS=-Djava.library.path=~/hadoop/lib/native' >> ~/.bashrc
 echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.bashrc
